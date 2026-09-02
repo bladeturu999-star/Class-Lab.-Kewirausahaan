e1634b710db9b1d75e659ebe99f9b1ee5a651662
bladeturu999-star <bladeturu999@gmail.com>
2026-09-02 11:50:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   supabaseUrl: 'https://uemwwcdzxudbaihszhpr.supabase.co',
-  supabaseAnonKey: 'sb_publishable_pwxoDUuyFVT8yzHXsQW6Pg_AaxQLal',
+  supabaseAnonKey: 'sb_publishable_pwxoDUuyFVT8yzHXsQW6Pg_AaxQLal-',
   className: 'Lab. Kewirausahaan II'
 };
